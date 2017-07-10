4960ab6910588aa8b3a6c7e13e45e37ed3f23a18
dreamlivemeng <dreamlivemeng@gmail.com>
2017-07-10 18:02:42 +0800

    增加描述
    增加描述

--- app/src/main/java/com/dreamlive/cn/sample/MainActivity.java
+++ app/src/main/java/com/dreamlive/cn/sample/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //throw ArithmeticException
         int i = 10 / 0;
     }
 }
